e3c77a0f66758e0b0e0f1396fee272c22e4c1ea1
skagmltn7 <skagmltn7@hanmail.net>
2024-01-07 04:15:18 +0900

    [add] @RequestBody

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,26 +1,13 @@
 package hello.itemservice.domain.item;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-import org.hibernate.validator.constraints.Range;
 
 @Data
 public class Item {
 
-    @NotNull(groups = UpdateCheck.class)
     private Long id;
-
-    @NotBlank(groups = {UpdateCheck.class, SaveCheck.class})
     private String itemName;
-
-    @NotNull(groups = {UpdateCheck.class, SaveCheck.class})
-    @Range(min = 1000, max = 1000000, groups = {UpdateCheck.class, SaveCheck.class})
     private Integer price;
-
-    @NotNull(groups = {UpdateCheck.class, SaveCheck.class})
-    @Max(value = 9999, groups = SaveCheck.class)
     private Integer quantity;
 
     public Item() {
